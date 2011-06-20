cc50c933105b69a1e980359202926b9c54109c22
Mathieu Marache <mathieu@marache.com>
2011-06-20 15:59:31 +0200

    created rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brouzouf::Application.config.secret_token = 'eb2dd788a7025c553781040383e0042e68f6e82a43a4d35c710b14f5e82603a32450d95b6dd5199a2fa482e30934aebd7f53961f26948bd3c5d036277a6287a7'
