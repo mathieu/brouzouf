43308ecfaefe58740c3160664925c7114ef780f4
Guillaume Besse <gui.besse@gmail.com>
2011-08-16 15:02:07 +0200

    Cosmetic : removing code dealing with xml responses

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -4,36 +4,18 @@ class BetsController < ApplicationController
   
   
   # GET /bets
-  # GET /bets.xml
   def index
     @bets = Bet.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @bets }
-    end
   end
 
   # GET /bets/1
-  # GET /bets/1.xml
   def show
     @bet = Bet.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @bet }
-    end
   end
 
   # GET /bets/new
-  # GET /bets/new.xml
   def new
     @bet = Bet.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @bet }
-    end
   end
 
   # GET /bets/1/edit
@@ -47,25 +29,19 @@ class BetsController < ApplicationController
   end
 
   # POST /bets
-  # POST /bets.xml
   def create
     @bet = Bet.new(params[:bet])
     # associate connected user with newly created bet
     @bet.user = current_user
 
-    respond_to do |format|
-      if @bet.save
-        format.html { redirect_to(@bet, :notice => 'Bet was successfully created.') }
-        format.xml  { render :xml => @bet, :status => :created, :location => @bet }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @bet.errors, :status => :unprocessable_entity }
-      end
+    if @bet.save
+      format.html { redirect_to(@bet, :notice => 'Bet was successfully created.') }
+    else
+      format.html { render :action => "new" }
     end
   end
 
   # PUT /bets/1
-  # PUT /bets/1.xml
   def update
     @bet = Bet.find(params[:id])
 
@@ -77,15 +53,11 @@ class BetsController < ApplicationController
   end
 
   # DELETE /bets/1
-  # DELETE /bets/1.xml
   def destroy
     @bet = Bet.find(params[:id])
     @bet.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(bets_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(bets_url)
   end
   
   
