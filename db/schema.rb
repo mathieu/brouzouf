0817ca8b27c7b91a72ef8eee8e84118a44a39b10
Mathieu MARACHE <mathieu@marache.com>
2011-08-17 17:54:05 +0200

    added confirmable migration for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110817095728) do
+ActiveRecord::Schema.define(:version => 20110817153645) do
 
   create_table "bets", :force => true do |t|
     t.string   "title"
@@ -54,8 +54,12 @@ ActiveRecord::Schema.define(:version => 20110817095728) do
     t.datetime "updated_at"
     t.string   "name"
     t.decimal  "brouzouf"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
