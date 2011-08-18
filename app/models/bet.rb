37a9a17e5fe08113fcb1ff7da4dc27bd68662679
Guillaume Besse <gui.besse@gmail.com>
2011-08-18 17:46:08 +0200

    Brouzouf distribution after bet closing

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,6 +3,8 @@ class Bet < ActiveRecord::Base
   has_many :choices
   has_one :choice
 
+  after_save :update_users_stats
+
   def expired?
     expiration_date < Time.now
   end
@@ -22,4 +24,30 @@ class Bet < ActiveRecord::Base
   def closed?
     state == 'CLOSED'
   end
+
+  private
+
+  def update_users_stats
+    logger.debug "Check if user just closed the bet then generate stats"
+
+    if(state_was == "OPEN" and closed?)
+      logger.debug "Bet is now CLOSED"
+
+       raise "A closed Bet need a choice" if !self.choice
+
+        Bid.where(:choice_id => self.choice.id).each do |bid|
+          logger.debug "1 more brouzouf for " + user.name
+          user = bid.user
+          !user.brouzouf ? user.brouzouf = 0 : user.brouzouf = user.brouzouf + 1
+          user.save
+        end
+
+    end
+
+  end
+
 end
+
+
+
+
