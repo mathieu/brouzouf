9deaa545a2ab8b8eb5588e119c7a4efae5761ba1
Mathieu MARACHE <mathieu@marache.com>
2011-10-14 00:19:39 +0200

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
-
-
 class UsersController < Devise::SessionsController
   def create
     super
   end
- 
- #user homepage 
- def index
-   logger.debug 'User HomePage actions'
-    @expired_bets_count = current_user.bets.where("expiration_date < ? AND state = 'OPEN' ", Time.now).count
- end
- 
+
+  #user homepage
+  def index
+    logger.debug 'User HomePage actions'
+    @expired_bets = current_user.bets.where("expiration_date < ? AND state = 'OPEN' ", Time.now)
+    @open_bets = current_user.bets.where("expiration_date > ? AND state = 'OPEN' ", Time.now)
+    @creation_bets = current_user.bets.where("state = 'CREATION' ", Time.now)
+    @closed_bets = current_user.bets.where("state = 'CLOSE' ", Time.now)
+  end
+
   def show
     logger.debug 'Public user display'
     @user = User.find(params[:id])
@@ -19,5 +20,8 @@ class UsersController < Devise::SessionsController
   def list
     @bidders = User.find(:all, :order => "brouzouf desc")
   end
- 
+
+  def expired_bets
+    return current_user.bets.where("expiration_date < ? AND state = 'OPEN' ", Time.now)
+  end
 end
